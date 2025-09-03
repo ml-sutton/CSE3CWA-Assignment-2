55b70e5e4376663a97a24c91d681b49302a84182
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-09-03 11:37:02 +1000

    finished dockerizing. Creating PR and merging into main

--- back-end/app/api/tabs/route.tsx
+++ back-end/app/api/tabs/route.tsx
@@ -1,4 +1,12 @@
-export async function GET()
-{
-  
+export async function GET() {
+
+}
+export async function DELETE() {
+
+}
+export async function PATCH() {
+
+}
+export async function POST() {
+
 }
