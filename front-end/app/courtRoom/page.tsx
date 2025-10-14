72eaad5ffe6572779cbca8fa925535517d66323f
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-10-15 10:06:33 +1100

    I might have things working! If this fails I am going to cry

---
+++ front-end/app/courtRoom/page.tsx
@@ -0,0 +1,9 @@
+import CourtRoomLayout from "../../components/courtRoom/courtRoomLayout";
+
+export default async function CourtRoomPage() {
+  return (
+    <main>
+      <CourtRoomLayout />
+    </main>
+  )
+} 
