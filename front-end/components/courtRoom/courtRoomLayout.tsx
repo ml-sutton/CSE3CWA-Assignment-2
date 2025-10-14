dbf71761e9d0812d2e9dce4a8faeeb8d73c8f4cb
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-10-15 09:45:35 +1100

    I have fought tooth and nail with these docker issues for weeks

---
+++ front-end/components/courtRoom/courtRoomLayout.tsx
@@ -0,0 +1,10 @@
+export default function CourtRoomLayout() {
+  return (
+    <section className="min-w-screen max-w-screen min-h-screen max-h-screen flex justify-center items-center">
+      <div className="w-4/5 px-8 py-4 rounded-xl flex gap-8 bg-blue-300">
+        <div className="">form</div>
+        <div className="">output code</div>
+      </div>
+    </section>
+  )
+}
