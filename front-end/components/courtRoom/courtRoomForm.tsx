9f9f3d89c750576d82af749d93bae207db7196c1
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-10-15 12:49:13 +1100

    pushing some of the work from today

--- front-end/components/courtRoom/courtRoomForm.tsx
+++ front-end/components/courtRoom/courtRoomForm.tsx
@@ -1,4 +1,11 @@
+"use client"
+
+import { useState } from "react"
+
 export default function CourtRoomForm() {
+  const [timer, setTimer] = useState<{ min: number, sec: number }>({ min: 5, sec: 30 });
+  const [difficulty, setDifficulty] = useState<{ scale: number, interval: number }>({ scale: 1.25, interval: 30 });
+  const [task, setTask] = useState<string>("Generate me a next.js page that shows card components for a product class")
   return (
     <div className="w-full">
       <div className="">
@@ -7,12 +14,21 @@ export default function CourtRoomForm() {
       <form action="">
         <div className="flex gap-4">
           <label htmlFor="">minutes</label>
-          <input type="text" />
+          <input type="number" min={0} max={15} value={timer.min} onChange={event => setTimer({ ...timer, min: Number(event.target.value) })} className="w-fit border-2" />
           <label htmlFor="">seconds</label>
-          <input type="text" />
+          <input type="number" min={0} max={59} value={timer.sec} onChange={event => setTimer({ ...timer, sec: Number(event.target.value) })} className="w-fit border-2" />
         </div>
-        <label htmlFor="">Initial task</label>
-        <input type="text" />
+        <div className="flex flex-col">
+          <div className="flex">
+            <label htmlFor="initial-task">Initial task</label>
+            <input type="text" id="initial-task" name="initial-task" placeholder="initial task" className="" />
+          </div>
+
+          <label htmlFor="">Difficulty scaler</label>
+          <input type="number" value={difficulty.scale} min={0.75} max={2.50} onChange={event => setDifficulty({ ...difficulty, scale: Number(event.target.value) })} className="w-fit border-2" />
+          <label htmlFor="">Difficulty interval</label>
+          <input type="number" value={difficulty.interval} onChange={event => setDifficulty({ ...difficulty, interval: Number(event.target.value) })} className="w-fit border-2" />
+        </div>
       </form>
     </div>
   )
---
+++ front-end/components/courtRoom/courtRoomOuput.tsx
@@ -0,0 +1,15 @@
+export default function CourtRoomOutput() {
+  return (
+    <div className="">
+      <div className="flex w-full">
+        <button className="w-1/2 border-2 rounded-tl-xl">Compile</button>
+        <button className="w-1/2 border-2 rounded-tr-xl" >Copy to clipboard</button>
+      </div>
+      <div className="bg-slate-100">
+        <code>
+
+        </code>
+      </div>
+    </div>
+  )
+}
